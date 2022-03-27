64a22852cc97f1614133561199d77d7a50967a8f
itntaba <itn.taba@gmail.com>
2022-03-27 16:12:02 +0430

    config added

---
+++ configs/nginx/bin/run.sh
@@ -0,0 +1,4 @@
+
+#!/bin/sh
+sed -i s/@@FPM_BACKEND@@/${FPM_BACKEND}/ /etc/nginx/conf.d/default.conf
+nginx -g "daemon off;"
---
+++ configs/varnish/varnish.vcl
@@ -0,0 +1,5 @@
+
+backend server_nginx_0 {
+    .host = "nginx";
+    .port = "80";
+}
